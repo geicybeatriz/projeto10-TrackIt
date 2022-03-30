844b6e352c07968b9cd05ce6c958cda94c1bba9d
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-03-30 15:09:08 -0300

    feat: header layout

---
+++ src/Components/Habits/index.jsx
@@ -0,0 +1,11 @@
+import {Container} from "./style.jsx";
+import Header from "../Header";
+
+export default function Habits(){
+    return (
+        <Container>
+            <Header/>
+        </Container>
+
+    );
+}
--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,5 +1,48 @@
+import styled from "styled-components";
+import userImg from "../../assets/img/Cayde6.jpeg"
+
 export default function Header(){
     return (
-        <h1>este é o cabeçalho</h1>
+        <Top>
+            <p>TrackIt</p>
+            <img src={userImg} alt="Foto de perfil"/>
+        </Top>
     );
 }
+
+const Top = styled.header`
+    width: 100%;
+    height: 70px;
+
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 3;
+
+    background-color: #126BA5;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 0 18px;
+
+    img{
+        width: 51px;
+        height: 51px;
+
+        border-radius: 50%;
+        background-size: cover;
+
+    }
+    p{
+        font-family: 'Playball';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 40px;
+        line-height: 49px;
+
+        color: #FFFFFF;
+    }
+`;
